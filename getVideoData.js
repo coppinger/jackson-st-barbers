9feac55830bb201d9a4fc6d895b4045e5f81294b
Charlie Coppinger <charles.coppinger@gmail.com>
2023-05-19 14:09:56 +1200

    key

--- getVideoData.js
+++ getVideoData.js
@@ -1,6 +1,7 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 // credentials you have generated when creating the service account. TIP: DO NOT check this into your Git repo and it to your .gitignore file
-const creds = require("/clientsecret.json");
+
+const creds = JSON.parse(SUPER_SECRET);
 
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
 const doc = new GoogleSpreadsheet(
