58f3139ee6b10656cf3f0897db053327f4df580e
Charlie Coppinger <charles.coppinger@gmail.com>
2023-05-20 18:46:46 +1200

    add sitemap to postbuild scripts

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://jacksonstbarbers.co.nz/",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
